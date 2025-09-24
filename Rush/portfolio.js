95c031bb163633ce5a03570b7747935a091c073e
jratphib <68100268@kmitl.ac.th>
2025-09-24 22:00:27 +0700

    Update portfolio.js

--- Rush/portfolio.js
+++ Rush/portfolio.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Smooth scroll for all anchor links within the portfolio screen
     document.querySelectorAll(".navbar a:not(#back-btn)").forEach(link => {
         link.addEventListener("click", e => {
             e.preventDefault();
@@ -7,13 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const targetElement = document.querySelector(targetId);
 
             if (targetElement) {
-                // Get the main portfolio screen container
                 const portfolioScreen = document.getElementById('portfolio');
-
-                // Calculate the target position with an offset for the fixed navbar
                 const offsetTop = targetElement.offsetTop - 100;
 
-                // Perform the smooth scroll
                 portfolioScreen.scrollTo({
                     top: offsetTop,
                     behavior: "smooth"
